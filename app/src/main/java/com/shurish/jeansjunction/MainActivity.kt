69bddc13c0d4cddf26d3871b6c46bc3a79defdc4
Rishabh Kumar <rishabh845401@gmail.com>
2024-07-20 18:22:43 +0530

    Adding files of the project

---
+++ app/src/main/java/com/shurish/jeansjunction/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shurish.jeansjunction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
